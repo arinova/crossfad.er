a35dd89e7a0b9a38ebdb8777aedc5ca7fb078e39
Ariane Joy Nova <arianenova@gmail.com>
2017-03-16 16:59:52 -0400

    playing with canvas

--- src/App.js
+++ src/App.js
@@ -5,33 +5,34 @@ import YouTube from 'react-youtube'
 import YouTubeSearch from './YouTubeSearch'
 import $ from 'jquery'
 import {isPlaying} from './utils'
+import Canvas from './Canvas'
 
 class App extends Component {
   constructor() {
     super()
-    
+
     this.state = {
       // audio slider params
       aFade: 100,
       aMin: 0,
       aMax: 200,
       aStep: 1,
-      
+
       // video slider params
       vFade: 50,
       vMin: 0,
       vMax: 100,
       vStep: 1,
-      
+
       // currently queued videos
       leftQ: [],
       rightQ: [],
-      
+
       // hold status of each video on state
       leftPlaying: false,
       rightPlaying: false
     }
-    
+
     // function bindings
     this.addToLeftPlayer = this.addToLeftPlayer.bind(this)
     this.addToRightPlayer = this.addToRightPlayer.bind(this)
@@ -50,7 +51,7 @@ class App extends Component {
     this.renderPlayPauseL = this.renderPlayPauseL.bind(this)
     this.renderPlayPauseR = this.renderPlayPauseR.bind(this)
   }
-  
+
   addToLeftPlayer(results) {
     if (document.getElementsByClassName('react-typeahead-usertext')[0].value.split(' ').join('').length) { // very ugly test of whether input has text
 
@@ -58,18 +59,18 @@ class App extends Component {
         id: results[0].id.videoId,
         title: results[0].snippet.title
       })
-      
+
       this.setState({
         leftQ: newQ
       })
-      
+
       console.log(this.state.leftQ)
     }
   }
-  
+
   addToRightPlayer(results) {
     if (document.getElementsByClassName('react-typeahead-usertext')[1].value.split(' ').join('').length) {
-      
+
       let newQ = this.state.rightQ.concat({
         id: results[0].id.videoId,
         title: results[0].snippet.title
@@ -82,15 +83,15 @@ class App extends Component {
       console.log(this.state.rightQ)
     }
   }
-  
+
   initializeVideo1(event) {
     this.video1 = event.target
   }
-  
+
   initializeVideo2(event) {
     this.video2 = event.target
   }
-  
+
   updateVideoInstance1(event) {
     this.video1 = event.target // will run on any player state change so we always have access to updated player information
     if (isPlaying(this.video1)) {
@@ -103,7 +104,7 @@ class App extends Component {
       })
     }
   }
-  
+
   updateVideoInstance2(event) {
     this.video2 = event.target
     if (isPlaying(this.video2)) {
@@ -116,7 +117,7 @@ class App extends Component {
       })
     }
   }
-  
+
   skipLeft(event) {
     event.preventDefault()
     this.video1.pauseVideo()
@@ -126,7 +127,7 @@ class App extends Component {
     })
     console.log(this.state.leftQ) // this lags a step behind, but functionality is fine
   }
-  
+
   skipRight(event) {
     event.preventDefault()
     this.video2.pauseVideo()
@@ -136,7 +137,7 @@ class App extends Component {
     })
     console.log(this.state.rightQ)
   }
-  
+
   playPauseVideo1(event) {
     event.preventDefault()
     if (isPlaying(this.video1)) {
@@ -145,7 +146,7 @@ class App extends Component {
       this.video1.playVideo()
     }
   }
-  
+
   playPauseVideo2(event) {
     event.preventDefault()
     if (isPlaying(this.video2)) {
@@ -154,7 +155,7 @@ class App extends Component {
       this.video2.playVideo()
     }
   }
-  
+
   aHandleChange(event) {
     if (event.target.value < 100) {
       this.video2.setVolume(event.target.value)
@@ -165,13 +166,13 @@ class App extends Component {
       this.video2.setVolume(100)
     }
   }
-  
+
   vHandleChange(event) {
     $(document).ready(() => {
       $('.youtube2').css('opacity', event.target.value/100)
     })
   }
-  
+
   renderPlayPauseL() {
     if (this.state.leftPlaying) {
       return (
@@ -193,7 +194,7 @@ class App extends Component {
       )
     }
   }
-  
+
   renderPlayPauseR() {
     if (this.state.rightPlaying) {
       return (
@@ -215,11 +216,11 @@ class App extends Component {
       )
     }
   }
-  
+
   renderMenuItemsL(queue) {
     let itemsForMap = queue,
       items = queue
-    
+
     return (
       <div className="btn-group dropdown-menu">
         {itemsForMap.map((item, idx) => (
@@ -236,16 +237,16 @@ class App extends Component {
               })
             }}>
             {`${items.length-idx-1}. ${items[items.length-idx-1].title}`}
-          </MenuItem>   
+          </MenuItem>
         ))}
       </div>
     )
   }
-  
+
   renderMenuItemsR(queue) {
     let itemsForMap = queue,
       items = queue
-    
+
     return (
       <div className="btn-group dropdown-menu">
         {itemsForMap.map((item, idx) => (
@@ -267,9 +268,14 @@ class App extends Component {
       </div>
     )
   }
-  
+
+  componentDidMount(){
+
+  }
+
+
   render() {
-    
+
     const options = {
       width: window.innerWidth - 30,
       height: window.innerHeight - 130,
@@ -286,15 +292,15 @@ class App extends Component {
         showInfo: 0
       }
     }
-    
+
     return (
       <div className="container-fluid">
-      
+
         {/* navbar */}
         <nav className="navbar navbar-default navbar-fixed-top">
           <div className="container-fluid">
             <div className="row">
-              
+
               {/* buttons1 */}
               <div className="col-sm-2 sm-2-top">
                 <div className="btn-group navbar-left" role="group">
@@ -307,12 +313,12 @@ class App extends Component {
                   </button>
                 </div>
               </div>
-              
+
               {/* slider1 */}
               <div className="col-sm-3">
                 <div className="a-slider">
                   <span className="navbar-top-text">a</span>
-                  <Slider 
+                  <Slider
                     value={this.state.aFade}
                     change={this.aHandleChange}
                     step={this.state.aStep}
@@ -322,7 +328,7 @@ class App extends Component {
                   />
                 </div>
               </div>
-              
+
               {/* search */}
               <div className="col-sm-1">
                 <div className="vid-search1">
@@ -330,26 +336,26 @@ class App extends Component {
                     apiKey='AIzaSyBOr-nJwESPXBlOSh-4-bf2R-ayOTUFVt4'
                     maxResults='5'
                     placeHolder='<- search'
-                    callback={this.addToLeftPlayer} 
+                    callback={this.addToLeftPlayer}
                   />
                 </div>
               </div>
-              
+
               <div className="col-sm-1">
                 <div className="vid-search2">
                   <YouTubeSearch
                     apiKey='AIzaSyBOr-nJwESPXBlOSh-4-bf2R-ayOTUFVt4'
                     maxResults='5'
                     placeHolder='search ->'
-                    callback={this.addToRightPlayer} 
+                    callback={this.addToRightPlayer}
                   />
                 </div>
               </div>
-              
+
               {/* slider2 */}
               <div className="col-sm-3">
                 <div className="v-slider">
-                  <Slider 
+                  <Slider
                     value={this.state.vFade}
                     change={this.vHandleChange}
                     step={this.state.vStep}
@@ -360,7 +366,7 @@ class App extends Component {
                   <span className="navbar-top-text">v</span>
                 </div>
               </div>
-              
+
               {/* buttons2 */}
               <div className="col-sm-2 sm-2-top">
                 <div className="btn-group navbar-right" role="group">
@@ -376,41 +382,47 @@ class App extends Component {
             </div>
           </div>
         </nav>
-        
+
         {/* players */}
         <div className="container-fluid">
-          <div className="youtube1">
-            <YouTube
-              videoId={this.state.leftQ.length ? this.state.leftQ[0].id : null}
-              opts={options}
-              onReady={this.initializeVideo1} // saves the video event 'video1' for later use
-              onPlay={this.updateVideoInstance1} // updates video event
-              onPause={this.updateVideoInstance1}  // updates video event
-              onStateChange={this.updateVideoInstance1}  // updates video event
-              onEnd={() => {
-                this.setState({
-                  leftQ: this.state.leftQ.slice(1)
-                })
-              }}
-            />
-          </div>
-          <div className="youtube2">
-            <YouTube
-              videoId={this.state.rightQ.length ? this.state.rightQ[0].id : null}
-              opts={options}
-              onReady={this.initializeVideo2}
-              onPlay={this.updateVideoInstance2} 
-              onPause={this.updateVideoInstance2}
-              onStateChange={this.updateVideoInstance2}
-              onEnd={() => {
-                this.setState({
-                  rightQ: this.state.rightQ.slice(1)
-                })
-              }}
-            />
+          <div>
+
+            <div className="youtube1">
+              <YouTube
+                videoId={this.state.leftQ.length ? this.state.leftQ[0].id : null}
+                opts={options}
+                onReady={this.initializeVideo1} // saves the video event 'video1' for later use
+                onPlay={this.updateVideoInstance1} // updates video event
+                onPause={this.updateVideoInstance1}  // updates video event
+                onStateChange={this.updateVideoInstance1}  // updates video event
+                onEnd={() => {
+                  this.setState({
+                    leftQ: this.state.leftQ.slice(1)
+                  })
+                }}
+              />
+            </div>
+            <div className="youtube2">
+              <YouTube
+                videoId={this.state.rightQ.length ? this.state.rightQ[0].id : null}
+                opts={options}
+                onReady={this.initializeVideo2}
+                onPlay={this.updateVideoInstance2}
+                onPause={this.updateVideoInstance2}
+                onStateChange={this.updateVideoInstance2}
+                onEnd={() => {
+                  this.setState({
+                    rightQ: this.state.rightQ.slice(1)
+                  })
+                }}
+              />
+            </div>
+
+            <Canvas/>
+
           </div>
         </div>
-        
+
         {/* footer */}
         <nav className="navbar navbar-default navbar-fixed-bottom">
           <div className="container-fluid">
